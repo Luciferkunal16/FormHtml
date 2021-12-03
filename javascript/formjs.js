135aa9cfc9f732afec6256f4c6294597cf64395c
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-12-03 21:16:05 +0530

    Added UC5:Password validation is updated

--- javascript/formjs.js
+++ javascript/formjs.js
@@ -7,11 +7,23 @@ text.oninput = function () {
 //email validation
 const emailError = document.querySelector(".email-error");
 email.oninput = function () {
-  let emailRegex = RegExp(
-    "^[a-zA-Z0-9-_+]+(\\.?[a-zA-Z0-9-_]+)@[a-zA-Z0-9-_]+\\.[a-zA-Z]{2,}(\\.?[a-zA-Z-_]+)$"
-  );
+  let emailRegex = RegExp("^[a-zA-Z0-9-_+]+(\\.?[a-zA-Z0-9-_]+)@[a-zA-Z0-9-_]+\\.[a-zA-Z]{2,}(\\.?[a-zA-Z-_]+)$");
   if (emailRegex.test(email.value)) emailError.textContent = "";
   else emailError.textContent = "Email is Incorrect";
+};
+//Mobile Number validation
+const telError = document.querySelector(".tel-error");
+tel.oninput = function () {
+  let telRegex = RegExp("^91 [1-9][0-9]{9}$");
+  if (telRegex.test(tel.value)) telError.textContent = "";
+  else telError.textContent = "Mobile No. is Incorrect";
+};
+//Password validation
+const pwdError = document.querySelector(".pwd-error");
+pwd.oninput = function () {
+  let pwdRegex = RegExp("^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$");
+  if (pwdRegex.test(pwd.value)) pwdError.textContent = "";
+  else pwdError.textContent = "Enter a valid Password";
 };
 //salary validation
 const salary = document.querySelector("#salary");
